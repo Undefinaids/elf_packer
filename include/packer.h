afa40b2129c6fba5ead1ff5337687c242b836fc0
Nico <undefinaids@gmail.com>
2020-08-12 21:56:35 +0200

    Dummy push

--- include/packer.h
+++ include/packer.h
@@ -5,4 +5,20 @@
 #ifndef ELF_PACKER_INCLUDE_PACKER_H_
 #define ELF_PACKER_INCLUDE_PACKER_H_
 
+#include <elf.h>
+
+// Handle 64 and 32 bits elf ?
+
+typedef struct  elf_file_s {
+  void          *file;
+  Elf64_Ehdr    *ehdr;
+}               elf_file_t;
+
+
+// union elf64_file elf32_file ?
+
+// ONLINE MAN: http://manpagesfr.free.fr/man/man5/elf.5.html
+
+//Objectif -> Rajouter une section vide
+
 #endif //ELF_PACKER_INCLUDE_PACKER_H_
